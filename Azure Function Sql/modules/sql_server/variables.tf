d0a0148b6bb78f373a1c7d38d636c135c98a0c73
babumg1210 <babumg1210@outlook.com>
2025-01-24 07:00:07 +0530

    Update variables.tf

--- Azure Function Sql/modules/sql_server/variables.tf
+++ Azure Function Sql/modules/sql_server/variables.tf
@@ -1,29 +1,23 @@
 variable "sql_server_name" {
-  description = "The name of the SQL Server."
-  type        = string
-}
-
-variable "sql_database_name" {
-  description = "The name of the SQL Database."
-  type        = string
+  description = "Name of the SQL server"
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
+  description = "Name of the resource group"
 }
 
 variable "location" {
-  description = "The Azure region to create resources in."
-  type        = string
+  description = "Azure region for the resource group"
 }
 
-variable "sql_admin_username" {
-  description = "The SQL Server administrator username."
-  type        = string
+variable "administrator_login" {
+  description = "Administrator login for SQL server"
 }
 
-variable "sql_admin_password" {
-  description = "The SQL Server administrator password."
-  type        = string
+variable "administrator_password" {
+  description = "Administrator password for SQL server"
+}
+
+variable "database_name" {
+  description = "Name of the SQL database"
 }
