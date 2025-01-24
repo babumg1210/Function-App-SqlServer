1209c7068853bcf96fa8b2d8f70068a83ab0aed3
babumg1210 <babumg1210@outlook.com>
2025-01-24 10:09:10 +0530

    Update outputs.tf

--- Azure Function Sql/modules/function_app/outputs.tf
+++ Azure Function Sql/modules/function_app/outputs.tf
@@ -1,7 +1,9 @@
 output "function_app_name" {
   value = azurerm_function_app.this.name
 }
-
+output "function_app_id" {
+  value = azurerm_function_app.this.id
+}
 output "function_app_identity" {
   value = azurerm_function_app.this.identity[0].principal_id
 }
