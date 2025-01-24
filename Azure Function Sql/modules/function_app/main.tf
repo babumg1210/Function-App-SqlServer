f28560329a3f7569b08b6a2aaf6eeda3f15aefb8
babumg1210 <babumg1210@outlook.com>
2025-01-24 09:33:07 +0530

    Update main.tf

--- Azure Function Sql/modules/function_app/main.tf
+++ Azure Function Sql/modules/function_app/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_function_app" "this" {
   name                       = var.function_app_name
   location                   = var.location
   resource_group_name        = var.resource_group_name
-  app_service_plan_id        = azurerm_app_service_plan.this.id
+  app_service_plan_id        = var.app_service_plan_id
   storage_account_name       = azurerm_storage_account.this.name
   storage_account_access_key = azurerm_storage_account.this.primary_access_key
   os_type                    = "linux"
