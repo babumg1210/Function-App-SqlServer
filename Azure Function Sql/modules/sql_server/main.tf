961340f33b016c4db29b2c0cfa3fb9bc55c64e52
babumg1210 <babumg1210@outlook.com>
2025-01-24 10:14:13 +0530

    Update main.tf

--- Azure Function Sql/modules/sql_server/main.tf
+++ Azure Function Sql/modules/sql_server/main.tf
@@ -11,5 +11,5 @@ resource "azurerm_mssql_database" "this" {
   name                = var.database_name
  # resource_group_name = var.resource_group_name
   server_id           = azurerm_mssql_server.this.id
-  requested_service_objective_name = "S1"
+ # requested_service_objective_name = "S1"
 }
