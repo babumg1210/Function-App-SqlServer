3c4cefe9b6c7f1d524d91eede2980b0c0903524e
babumg1210 <babumg1210@outlook.com>
2025-01-24 06:59:42 +0530

    Update main.tf

--- Azure Function Sql/modules/sql_server/main.tf
+++ Azure Function Sql/modules/sql_server/main.tf
@@ -1,20 +1,15 @@
-resource "azurerm_mssql_server" "this" {
+resource "azurerm_mssql_server" "sql_server" {
   name                         = var.sql_server_name
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = "12.0"
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_password
 }
 
-resource "azurerm_mssql_database" "this" {
-  name                = var.sql_database_name
+resource "azurerm_mssql_database" "sql_database" {
+  name                = var.database_name
   resource_group_name = var.resource_group_name
-  location            = var.location
-  server_name         = azurerm_mssql_server.this.name
-  sku {
-    name     = "S0"
-    tier     = "Standard"
-    capacity = 10
-  }
+  server_id           = azurerm_mssql_server.sql_server.id
+  requested_service_objective_name = "S1"
 }
